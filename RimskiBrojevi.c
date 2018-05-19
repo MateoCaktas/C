41100a614d4862a1fe6652e418966eb1fc8606dc
MateoCaktas <37536819+MateoCaktas@users.noreply.github.com>
2018-05-19 22:49:57 +0200

    Add files via upload

---
+++ RimskiBrojevi.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <string.h>
+
+int PrebaciUArapske(char[]);
+int Provjeri(int,int);
+int Zbroji(int);
+int PrebaciBroj(char);
+
+int main()
+{
+    char niz[20]= {};
+    int rezultat=0;
+
+    printf("Unesi niz znakova:");
+    
+    scanf("%s",niz);
+
+    printf("\n%s\n",niz);
+
+    rezultat = PrebaciUArapske(niz);
+
+    printf("\nRezultat : %d\n",rezultat);
+    return 0;
+}
+
+
+int PrebaciUArapske(char niz[])
+{
+    int duzina = strlen(niz);
+    int i,final = 0;
+    int rezultat=0;
+    int broj1,broj2,broj;
+
+    printf("\nDuljina = %d\n",duzina);
+
+    for(i = 0 ; i<duzina;i++)
+    {
+        broj1 = PrebaciBroj(niz[i]);
+        if(i<duzina)
+        broj2 = PrebaciBroj(niz[i+1]);
+
+        broj = Provjeri(broj1,broj2);
+
+        if(broj1 < broj2)
+        i++;
+            
+        final = Zbroji(broj);
+        rezultat +=final;
+    }
+
+    return rezultat;
+}
+
+int Provjeri(int a, int b)
+{
+    if(a < b)
+    return b-a;
+
+    else return a;
+}
+
+int Zbroji(int num)
+{
+    static int zbroj = 0;
+    return num+zbroj;
+}
+
+int PrebaciBroj(char slovo)
+{
+    int broj;
+    toupper(slovo);
+    switch(slovo)
+    {
+    case 'M':
+        broj = 1000;
+        break;
+    case 'D':
+        broj = 500;
+        break;
+    case 'C':
+        broj = 100;
+        break;
+    case 'L':
+        broj = 50;
+        break;
+    case 'X':
+        broj = 10;
+        break;
+    case 'V':
+        broj = 5;
+        break;
+    case 'I':
+        broj = 1;
+        break;
+        }
+
+        return broj;
+}
